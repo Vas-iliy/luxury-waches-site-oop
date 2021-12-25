6cb5649890db80dfd0f8f728ecd2ae258f798753
Vasiliy <kkolyasev1228@gmail.com>
2021-12-25 12:24:16 +0400

    currency

--- app/controllers/App.php
+++ app/controllers/App.php
@@ -2,6 +2,7 @@
 
 namespace app\controllers;
 
+use app\widgets\currency\Currency;
 use luxury\base\Controller;
 use app\models\AppModel;
 
@@ -11,5 +12,10 @@ class App extends Controller
     {
         parent::__construct($route);
         new AppModel();
+        setcookie('currency', 'EUR', time() + 3600*24*7, '/');
+        \luxury\App::$app->setProperty('currencies', Currency::getCurrencies());
+        \luxury\App::$app->setProperty('currency', Currency::getCurrency(
+            \luxury\App::$app->getProperty('currencies')));
+        debug(\luxury\App::$app->getProperties());
     }
 }
