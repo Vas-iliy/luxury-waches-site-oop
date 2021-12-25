1fe86a6599597edca4e29e161d3fb2191237475c
Vasiliy <kkolyasev1228@gmail.com>
2021-12-25 11:42:08 +0400

    product

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -7,8 +7,10 @@ use luxury\Cache;
 class MainController extends App
 {
     public function indexAction() {
-        $posts = \R::findAll('test');
-        $this->setMeta('Главная страница', 'fdfdfd', 'lalala');
+        $brands = \R::find('brands', 'LIMIT 3');
+        $hits = \R::find('products', "hit = '1' AND status = '1' LIMIT 8");
+        $this->setMeta('Главная страница');
+        $this->set(compact('brands', 'hits'));
 
     }
 }
