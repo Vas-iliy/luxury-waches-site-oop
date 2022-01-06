3c7bd6afeb5e5fb2c85a5243f903750e54a5f121
Vasiliy <kkolyasev1228@gmail.com>
2022-01-06 20:02:33 +0400

    saveOrder

--- app/models/Order.php
+++ app/models/Order.php
@@ -3,11 +3,20 @@
 
 namespace app\models;
 
+use luxury\App;
 
 class Order extends AppModel
 {
     public static function saveOrder($data) {
-
+        $order = new Order();
+        $order->attributes = [
+            'id_user' => $data['id_user'],
+            'currency' => App::$app->getProperty('currency')['code'],
+            'note' => $data['note']
+        ];
+        $idOrder = $order->save('orders');
+        self::saveOrderProduct($idOrder);
+        return $idOrder;
     }
 
     public static function saveOrderProduct($idOrder) {
